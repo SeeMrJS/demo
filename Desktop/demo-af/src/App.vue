122049eb7700a85a1687941d976a064b3318c40b
seemr <1210389056@qq.com>
2022-04-06 11:41:29 +0800

    start

---
+++ Desktop/demo-af/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Brace from './components/minesweeper/index.vue'
+</script>
+
+<template>
+  <div>seemr</div>
+  <Brace/>
+</template>
+
+<style>
+html,body {
+  height: 100%;
+  width: 100%;
+  background-color: #000;
+  margin: 0;
+  padding: 0;
+}
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
